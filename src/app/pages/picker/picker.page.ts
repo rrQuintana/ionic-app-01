9150209e9a70af44e4f588deac90a59b25bed22a
Roberto Quintana <rrquintana7@gmail.com>
2024-02-20 19:33:35 -0600

    dateTime picker + select

--- src/app/pages/picker/picker.page.ts
+++ src/app/pages/picker/picker.page.ts
@@ -1,15 +1,97 @@
 import { Component, OnInit } from '@angular/core';
+import { PickerController } from '@ionic/angular';
 
 @Component({
   selector: 'app-picker',
   templateUrl: './picker.page.html',
   styleUrls: ['./picker.page.scss'],
 })
-export class PickerPage implements OnInit {
+export class PickerPage {
 
-  constructor() { }
+  constructor(
+    private _pickerCtrl: PickerController
+  ) { }
 
-  ngOnInit() {
+  async selectPizza() {
+    const picker = await this._pickerCtrl.create(
+      {
+        columns: [
+          {
+            name: 'Pizza',
+            options: [
+              { text: 'Pepperoni', value: 'pepperoni' },
+              { text: 'Sausage', value: 'sausage' },
+              { text: 'Mushroom', value: 'mushroom' },
+              { text: 'Pepper', value: 'pepper' },
+              { text: 'Onion', value: 'onion' },
+              { text: 'Cheese', value: 'cheese' }
+            ]
+          }
+        ],
+        buttons: [
+          {
+            text: 'Cancel',
+            role: 'cancel'
+          },
+          {
+            text: 'Ok'
+          }
+        ]
+      }
+
+      )
+      await picker.present();
+  }
+
+  async selectBurguer() {
+    const picker = await this._pickerCtrl.create(
+      {
+        columns: [
+          {
+            name: 'Burguer',
+            options: [
+              { text: 'Chicken', value: 'chicken' },
+              { text: 'Beef', value: 'beef' },
+              { text: 'Pork', value: 'pork' },
+              { text: 'Fish', value: 'fish' },
+              { text: 'Vegan', value: 'vegan' }
+            ]
+          },
+          {
+            name: 'Toppings',
+            options: [
+              { text: 'Lettuce', value: 'lettuce' },
+              { text: 'Tomato', value: 'tomato' },
+              { text: 'Onion', value: 'onion' },
+              { text: 'Pickles', value: 'pickles' },
+              { text: 'Cheese', value: 'cheese' }
+            ]
+          },
+          {
+            name: 'Sauce',
+            options: [
+              { text: 'Ketchup', value: 'ketchup' },
+              { text: 'Mustard', value: 'mustard' },
+              { text: 'Mayo', value: 'mayo' },
+              { text: 'BBQ', value: 'bbq' },
+              { text: 'Ranch', value: 'ranch' }
+            ]
+          
+          }
+        ],
+        buttons: [
+          {
+            text: 'Cancel',
+            role: 'cancel'
+          },
+          {
+            text: 'Ok'
+          }
+        ]
+      }
+
+      )
+      await picker.present();
   }
 
 }
